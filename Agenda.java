ad80e7e5c9a22a10d3e6cd4fb222bb87e8b5de69
joellozanobarbancho <joellozanobarbancho@elpuig.xeill.net>
2025-01-15 19:36:13 +0100

    Add files via upload

--- Agenda.java
+++ Agenda.java
@@ -1,25 +1,130 @@
+import java.util.Scanner;
+
 public class Agenda {
     Contact[] contacts = new Contact[100];
     int id = 0;
-    static void showMenu() {
-        System.out.println("""
-                         Welcome to the agenda, please choose an option:
-                         1. Create a new contact.
-                         2. Search for existing contacts.
-                         3. Update an existing contact.
-                         4. Delete an existing contact.
-                         5. Exit."""
-        );
-    }
-    static void showOptions(int input) {
+    void showContact(Contact c) {
+        if (c == null) return;
         System.out.println(
-                "You selected the option to " + switch (input) {
-            case 1 -> "create new contact\n";
-            case 2 -> "search for existing contact\n";
-            case 3 -> "update an existing contact\n";
-            case 4 -> "delete an existing contact\n";
-            case 5 -> "exit\n";
-            default -> "do nothing\n";
-        });
+                "ID: " + c.id +
+                        "\nNAME: " + c.name +
+                        "\nSURNAME: " + c.surname +
+                        "\nTEL: " + c.tel +
+                        "\nEMAIL: " + c.email
+        );
+        System.out.println();
+    }
+    void createContact(Scanner sc) {
+        if (sc == null) return;
+        System.out.println("Insert new contact's name:");
+        String name = sc.next();
+        System.out.println("Insert contact's surname: ");
+        String surname = sc.next();
+        System.out.println("Insert contact's phone:");
+        String tel = sc.next();
+        System.out.println("Insert contact's email:");
+        String email = sc.next();
+        this.contacts[this.id] = new Contact(this.id, name, surname, tel, email);
+        System.out.println("\nNew contact created!\n");
+        showContact(this.contacts[this.id]);
+    }
+    void searchContact(Scanner sc) {
+        if (sc == null) return;
+        System.out.println("""
+                Choose a search option:
+                1. Search all.
+                2. Search by name.
+                3. Search by surname.
+                4. Search by phone.
+                5. Search by email.
+                6. Back."""
+        );
+        switch (sc.nextInt()) {
+            case 1 -> searchAll();
+            case 2 -> searchByName(sc);
+            case 3 -> searchBySurname(sc);
+            case 4 -> searchByPhone(sc);
+            case 5 -> searchByEmail(sc);
+            //default -> go back;
+        }
+    }
+    void updateContact(Scanner sc) {
+        if (sc == null) return;
+        System.out.println("Insert contact's ID:");
+        int id = sc.nextInt();
+        if (id < this.id) createContact(sc);
+        else System.out.println("Error: Invalid ID\n");
+        System.out.println('\n');
+    }
+    void deleteContact(Scanner sc) {
+        if (sc == null) return;
+        System.out.println("Insert contact's ID:");
+        int id = sc.nextInt();
+        if (id < this.id) {
+            this.contacts[this.id] = null;
+            System.out.println("Contact deleted!\n");
+        }
+        else System.out.println("Error: Invalid ID\n");
+    }
+
+    private void searchAll() {
+        for (Contact ct : this.contacts)
+            this.showContact(ct);
+    }
+    private void searchByName(Scanner sc) {
+        if (sc == null) return;
+        System.out.println("Insert contact's name: ");
+        String s = sc.next();
+        boolean found = false;
+        for (Contact ct : this.contacts) {
+            if (ct.name.contains(s)) {
+                found = true;
+                this.showContact(ct);
+                break;
+            }
+        }
+        if (!found) System.out.println("Contact not found :(");
+    }
+    private void searchBySurname(Scanner sc) {
+        if (sc == null) return;
+        System.out.println("Insert contact's name: ");
+        String s = sc.next();
+        boolean found = false;
+        for (Contact ct : this.contacts) {
+            if (ct.surname.contains(s)) {
+                found = true;
+                this.showContact(ct);
+                break;
+            }
+        }
+        if (!found) System.out.println("Contact not found :(");
+    }
+    private void searchByPhone(Scanner sc) {
+        if (sc == null) return;
+        System.out.println("Insert contact's name: ");
+        String s = sc.next();
+        boolean found = false;
+        for (Contact ct : this.contacts) {
+            if (ct.tel.contains(s)) {
+                found = true;
+                this.showContact(ct);
+                break;
+            }
+        }if (!found) System.out.println("Contact not found :(");
+    }
+    private void searchByEmail(Scanner sc) {
+        if (sc == null) return;
+        System.out.println("Insert contact's name: ");
+        String s = sc.next();
+        boolean found = false;
+        for (Contact ct : this.contacts) {
+            if (ct.email.contains(s)) {
+                found = true;
+                this.showContact(ct);
+                break;
+            }
+        }
+        if (!found) System.out.println("Contact not found :(");
     }
 }
+
--- Main.java
+++ Main.java
@@ -2,19 +2,40 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    private static void showMenu() {
+        System.out.println("""
+                         Welcome to the agenda, please choose an option:
+                         1. Create a new contact.
+                         2. Search for existing contacts.
+                         3. Update an existing contact.
+                         4. Delete an existing contact.
+                         5. Exit."""
+        );
+    }
+    private static void showOptions(int input) {
+        System.out.println(
+                "You selected the option to " + switch (input) {
+                    case 1 -> "create new contact\n";
+                    case 2 -> "search for existing contact\n";
+                    case 3 -> "update an existing contact\n";
+                    case 4 -> "delete an existing contact\n";
+                    case 5 -> "exit\n";
+                    default -> "do nothing\n";
+                });
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
         Agenda a = new Agenda();
         while (true) {
-            Agenda.showMenu();
+            showMenu();
             int input = sc.nextInt();
-            Agenda.showOptions(input);
+            showOptions(input);
             switch (input) {
-                case 1 -> Contact.createContact(sc, a.contacts, a.id++);
-                case 2 -> Contact.searchContact(sc, a.contacts);
-                case 3 -> Contact.updateContact(sc, a.contacts, a.id);
-                case 4 -> Contact.deleteContact(sc, a.contacts, a.id);
+                case 1 -> a.createContact(sc);
+                case 2 -> a.searchContact(sc);
+                case 3 -> a.updateContact(sc);
+                case 4 -> a.deleteContact(sc);
                 case 5 -> {
                     return;
                 }
