f18a23e59f3b921529533bb4978c1fcc5e8bf17b
joellozanobarbancho <joellozanobarbancho@elpuig.xeill.net>
2025-03-13 20:12:28 +0100

    Maven set up

--- src/test/java/AgendaTest.java
+++ src/test/java/AgendaTest.java
@@ -1,4 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 class AgendaTest {
 
     @org.junit.jupiter.api.Test
