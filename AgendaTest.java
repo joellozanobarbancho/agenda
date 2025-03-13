90b0326f4ca8c284a7e75a1d616d4725de86f60c
joellozanobarbancho <joellozanobarbancho@elpuig.xeill.net>
2025-03-13 19:56:51 +0100

    AgendaTest created

---
+++ AgendaTest.java
@@ -0,0 +1,35 @@
+import static org.junit.jupiter.api.Assertions.*;
+class AgendaTest {
+
+    @org.junit.jupiter.api.Test
+    void createContact() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateContact() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteContact() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchByName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchBySurname() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchByPhone() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchByEmail() {
+    }
+}
